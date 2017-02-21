f928ec8ea15bf0c999b542b5fe6a2b59b6abb7be
doniamekni <donia.mekni@notchapp.net>
2017-02-21 21:21:10 +0100

    changed route

--- routes/categories.js
+++ routes/categories.js
@@ -5,6 +5,6 @@ var categoriesCtrl = require('../controllers/categoryCtrl');
 
 router.get('/', categoriesCtrl.getCategories);
 router.get('/:id', categoriesCtrl.getCategory);
-router.get('/institutions/:id', categoriesCtrl.getInstitutions);
+router.get('/:id/institutions', categoriesCtrl.getInstitutions);
 
 module.exports = router;
